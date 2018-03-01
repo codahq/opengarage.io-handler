8364b39e6f6372a54a65d5f1775b59fa1a9ff9fb
codahq <codahq@users.noreply.github.com>
2018-03-01 01:20:05 -0700

    Update OpenGarage.io-smartapp.groovy
    + fixed a bug where the wrong method would be called in the mapping
    + cleaned up some leftover junk from the example

--- OpenGarage.io-smartapp.groovy
+++ OpenGarage.io-smartapp.groovy
@@ -1,18 +1,29 @@
 /**
- *  OpenGarage SmartApp
- *
  *  Copyright 2018 Ben Rimmasch
+ *
+ * 
+ *  This SmartApp code can be found at:
+ *  https://github.com/codahq/opengarage.io-handler
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
  *
  */
 
 definition(
-    name: "HTTP Open/Close Endpoint",
+    name: "OpenGarage HTTP Refresh Endpoint",
     namespace: "me.bendy.opengarage",
     author: "Ben Rimmasch",
-    description: "Creates an HTTP motion endpoint for your OpenGarage",
+    description: "Creates an HTTP refresh endpoint for your OpenGarage",
     category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/intruder_motion-cam.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/intruder_motion-cam@2x.png"
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/garage_contact.png.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/garage_contact@2x.png"
 )
 
 preferences {
@@ -40,7 +51,7 @@ def updated() {
 mappings {
     path("/needs_update") {
         action: [
-            GET: "openStatus"
+            GET: "doRefresh"
         ]
     }
 }
@@ -56,11 +67,11 @@ def generateURL() {
 }
 
 def viewURL() {
-    dynamicPage(name: "viewURL", title: "HTTP Motion Endpoint", install:!resetOauth, nextPage: resetOauth ? "viewURL" : null) {
+    dynamicPage(name: "viewURL", title: "OpenGarage HTTP Refresh Endpoint", install:!resetOauth, nextPage: resetOauth ? "viewURL" : null) {
         section() {
             generateURL() 
-            paragraph "Open: https://graph.api.smartthings.com/api/smartapps/installations/${app.id}/open?access_token=${state.accessToken}"
-            paragraph "Close: https://graph.api.smartthings.com/api/smartapps/installations/${app.id}/close?access_token=${state.accessToken}"
+            paragraph "Refresh: https://graph.api.smartthings.com/api/smartapps/installations/${app.id}/needs_update?access_token=${state.accessToken}"
+            paragraph "Note that your base URL may not be https://graph.api.smartthings.com.  Verify your base API URL!"
         }
     }
 }
